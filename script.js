3e1608d15d90a1711fdfcf3b65eb6ebeda1e2256
Prakriti Singh <k92993254@gmail.com>
2026-04-12 19:53:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var main = document.querySelector("#main")
+
 var cursor = document.querySelector("#cursor")
-main.addEventListener("mousemove",function(dets){
+document.addEventListener("mousemove",function(dets){
     gsap.to(cursor,{
         x:dets.x,
         y:dets.y,
